a82e86b2986c0c21bb3feb497c6aebc8f7b22460
Dava1991 <134635344+Dava1991@users.noreply.github.com>
2023-12-12 11:34:27 +0200

    8

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,28 +1,17 @@
-import { FriendListItem } from './FriendListItem/FriendListItem';
-import { StyledList } from './FriendList.styled';
-import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+import FriendListItem from './FriendListItem';
 
-export const FriendList = ({ friends }) => {
-  return (
-    <StyledList>
-      {friends.map(friend => (
-        <FriendListItem key={friend.id} friend={friend} />
-      ))}
-    </StyledList>
-  );
-};
+const FriendList = ({ friends }) => (
+  <ul className={css.friendList}>
+    {friends.map(({ avatar, name, isOnline, id }) => (
+      <FriendListItem
+        avatar={avatar}
+        name={name}
+        isOnline={isOnline}
+        key={id}
+      />
+    ))}
+  </ul>
+);
 
-FriendList.defaultProps = {
-  friends: [],
-};
-
-FriendList.propTypes = {
-  friends: PropTypes.arrayOf(
-    PropTypes.shape({
-      isOnline: PropTypes.bool.isRequired,
-      avatar: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      id: PropTypes.number.isRequired,
-    })
-  ),
-};
+export default FriendList;
